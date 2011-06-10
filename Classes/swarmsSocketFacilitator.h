f6b9f73611f15d942939e84360570a6458c79965
Alexander H. List <alist@listprojects.com>
2011-06-10 15:21:13 -0400

    committed basic rev of swarmsBBDriver for iOS

---
+++ Classes/swarmsSocketFacilitator.h
@@ -0,0 +1,19 @@
+//
+//  swarmsSocketFacilitator.h
+//  swarmsBBDriver
+//
+//  Created by Alexander List on 6/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+static NSString * const remoteDriveIP	= @"192.168.1.100";
+static NSString * const remoteDrivePort	= @"7337";
+
+@interface swarmsSocketFacilitator : NSObject {
+
+	
+}
+
+@end
---
+++ Classes/swarmsSocketFacilitator.m
@@ -0,0 +1,14 @@
+//
+//  swarmsSocketFacilitator.m
+//  swarmsBBDriver
+//
+//  Created by Alexander List on 6/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "swarmsSocketFacilitator.h"
+
+
+@implementation swarmsSocketFacilitator
+
+@end
